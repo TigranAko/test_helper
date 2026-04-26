efe925297b7603c52f67a060ad4e2bb8be506ad8
Tigran <tigranakopyan2007.ru@gmail.com>
2026-04-26 17:16:28 +0300

    feat(api): add file upload handler

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+import aiofiles
+from fastapi import FastAPI, UploadFile
 from uvicorn import run
 
 app = FastAPI(title="Тест")
@@ -7,6 +8,17 @@ app = FastAPI(title="Тест")
 @app.get("/")
 def main():
     return "Hello from test-helper!"
+
+
+@app.post("/file")
+async def upload_file(test_file: UploadFile):
+    print(test_file.content_type)
+    print(test_file.filename)
+    async with aiofiles.open(f"files/{test_file.filename}", "wb") as file:
+        while chunk := await test_file.read(1024):
+            await file.write(chunk)
+            # TODO: файл можно не сохранять
+    return {"filename": test_file.filename}
 
 
 if __name__ == "__main__":
