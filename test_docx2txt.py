212de77c3a2e8e22fa7eb2987cfd56ae13c99818
Tigran <tigranakopyan2007.ru@gmail.com>
2026-04-30 11:08:10 +0300

    test(docx2txt): reed images from the file

--- test_docx2txt.py
+++ test_docx2txt.py
@@ -1,5 +1,8 @@
 import docx2txt
+# TODO: Таблицы из файла *.docx плохо созраняются (обычным сплошным текстом)
 
+# text = docx2txt.process("./files/text_01_03.docx")
+# print(text)
 
-text = docx2txt.process("./files/text_01_03.docx")
-print(text)
+text = docx2txt.process("./files/DM4.docx", "files/")
+print(text)  # 18 Задание с таблицами и графом (картинка)
